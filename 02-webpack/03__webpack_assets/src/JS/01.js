84adcb137e659722a1a340eee7fcfeb5aee67b49
czx <1546158360@qq.com>
2022-03-28 08:37:51 +0800

    :construction: 学习ing

--- 02-webpack/03__webpack_assets/src/JS/01.js
+++ 02-webpack/03__webpack_assets/src/JS/01.js
@@ -2,14 +2,18 @@
  * @Author: czx
  * @Date: 2022-03-26 16:40:36
  * @LastEditors: czx
- * @LastEditTime: 2022-03-26 18:26:02
+ * @LastEditTime: 2022-03-28 08:33:12
  * @description: 
  */
 import "../styles/01.css"
 import "../styles/02.less"
+import testimg from "../assets/帅哥.jpg"
 export const createElement = () => {
   const elemen = document.createElement("div");
+  const elemen2 = document.createElement("img");
   elemen.innerHTML = "Hello Spirit";
+  elemen2.src = testimg;
   const body = document.querySelector("body");
   body.append(elemen);
+  body.append(elemen2);
 }
