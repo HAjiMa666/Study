de88397e02c60c744dfe300d0ca30c5e13fd51c1
czx <1546158360@qq.com>
2022-03-29 08:38:28 +0800

    :construction: devServer Studying

---
+++ 02-webpack/07_webpack-devServer/src/vue/HelloWorld.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: czx
+ * @Date: 2022-03-29 07:17:45
+ * @LastEditors: czx
+ * @LastEditTime: 2022-03-29 07:59:37
+ * @description: 
+-->
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello World组件123"
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: aquamarine;
+}
+</style>
