df824575e209d47bb5b400bb8c0dd0a7106f8ebc
czx <1546158360@qq.com>
2022-03-31 08:31:40 +0800

    :construction: vue-cli,vite studying

---
+++ 02-webpack/10-vite/src/main.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: czx
+ * @Date: 2022-03-31 07:50:13
+ * @LastEditors: czx
+ * @LastEditTime: 2022-03-31 07:54:30
+ * @description: 
+ */
+import { getName } from "./01"
+
+console.log(getName());
