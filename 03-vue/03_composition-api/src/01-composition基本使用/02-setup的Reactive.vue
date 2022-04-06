a3064979800e8bdb667a24ae196282a6ad888f8e
czx <1546158360@qq.com>
2022-04-06 08:15:41 +0800

    :construction: vue3-composition学习中

---
+++ 03-vue/03_composition-api/src/01-composition基本使用/02-setup的Reactive.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: czx
+ * @Date: 2022-04-06 07:22:46
+ * @LastEditTime: 2022-04-06 07:37:57
+ * @LastEditors: czx
+ * @Description: 
+-->
+<template>
+  <div>
+    <button @click="increment">添加</button>
+    <h1>当前计数:{{ state.counter }}</h1>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const state = reactive({
+      counter: 0,
+    });
+    const increment = () => {
+      state.counter++;
+    };
+    return {
+      state,
+      increment,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
