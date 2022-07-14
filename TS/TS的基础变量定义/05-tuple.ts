644d00c12461a6e8e98d18bd8342edbc228fdd66
czx <70849018+HAjiMa666@users.noreply.github.com>
2022-07-14 20:47:35 +0800

    docs: JS系统课内容提交

--- TS/TS的基础变量定义/05-tuple.ts
+++ TS/TS的基础变量定义/05-tuple.ts
@@ -1,10 +1,11 @@
 /*
  * @Author: czx
  * @Date: 2022-05-13 11:13:53
- * @LastEditTime: 2022-05-13 11:54:42
+ * @LastEditTime: 2022-07-13 09:22:04
  * @LastEditors: czx
  * @Description: 元组类型
  */
 
+//: 元素数量和类型固定的数组类型
 
 const numberArr:[string,number,boolean]=["123",2222,true];
